1d8edd45471546c4b00163064884cd20da501c15
fersus <deriabin_85@mail.ru>
2023-07-05 18:31:14 +0300

    add swagger documentation

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 from sms.views import ClientViewSet, MailingViewSet, MessageViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Service notification',
+        default_version='v1',
+        description="Cервис управления рассылками API администрирования и получения статистики, в котором реализованы: "
+                    "методы создания новой рассылки, просмотра созданных и получения статистики по выполненным "
+                    "рассылкам, сам сервис отправки уведомлений на внешнее API, организовано тестирование кода",
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='deriabin_85@mail.ru'),
+        license=openapi.License(name='BSD License')
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
 
 router = routers.SimpleRouter()
 router.register(r'clients', ClientViewSet, basename='clients')
@@ -25,5 +42,8 @@ router.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('docs/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'
+         )
 ]
